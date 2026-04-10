StuffzEZ/StaffPlusPlus

Add 1.21.11 support

src/main/java/net/shortninja/staffplus/core/domain/staff/mode/listeners/ModeAdvancementListener.java
@@ -30,23 +30,22 @@ public class ModeAdvancementListener implements Listener {
         OnlinePlayerSession session = sessionManager.get(player);
         
         if (!session.isInStaffMode()) return;
-        if (!(player.getWorld().getGameRuleValue(GameRule.ANNOUNCE_ADVANCEMENTS) || announceGameruleDisabledByUs)) return;
         
         Advancement advancement = event.getAdvancement();
-        
-        // There is no better way to do it than to disable the gamerule and then enable it
-        announceGameruleDisabledByUs = true;
-        player.getWorld().setGameRule(GameRule.ANNOUNCE_ADVANCEMENTS, false);
-        
         for (String criteria : advancement.getCriteria()) {
             player.getAdvancementProgress(advancement).revokeCriteria(criteria);
         }
+        
+        if (!(player.getWorld().getGameRuleValue(GameRule.SHOW_ADVANCEMENT_MESSAGES) || announceGameruleDisabledByUs)) return;
+        // There is no better way to do it than to disable the gamerule and then enable it
+        announceGameruleDisabledByUs = true;
+        player.getWorld().setGameRule(GameRule.SHOW_ADVANCEMENT_MESSAGES, false);
         
         // Enable gamerule after the event
         new BukkitRunnable() {
             @Override
             public void run() {
-                player.getWorld().setGameRule(GameRule.ANNOUNCE_ADVANCEMENTS, true);
+                player.getWorld().setGameRule(GameRule.SHOW_ADVANCEMENT_MESSAGES, true);
                 announceGameruleDisabledByUs = false;
             }
         }.runTask(StaffPlusPlus.get());

CADIndie/StaffPlusPlus-Folia

(cherry picked from commit cdf18d55e17dc7502e9d7005a7fb83e81faf39e9)

src/main/java/net/shortninja/staffplus/core/domain/staff/mode/listeners/ModeAdvancementListener.java
@@ -1,4 +1,4 @@
-package net.shortninja.staffplus.core.domain.staff.mode.listeners;
+package net.shortninja.staffplus.core.domain.staff.mode;
 
 import be.garagepoort.mcioc.tubingbukkit.annotations.IocBukkitListener;
 import me.nahu.scheduler.wrapper.runnable.WrappedRunnable;
@@ -32,26 +32,26 @@ public class ModeAdvancementListener implements Listener {
         OnlinePlayerSession session = sessionManager.get(player);
 
         if (!session.isInStaffMode()) return;
-        if (!(player.getWorld().getGameRuleValue(GameRule.ANNOUNCE_ADVANCEMENTS) || announceGameruleDisabledByUs)) return;
 
         Advancement advancement = event.getAdvancement();
-
-        // There is no better way to do it than to disable the gamerule and then enable it
-        announceGameruleDisabledByUs = true;
-
-        Bukkit.getGlobalRegionScheduler().execute(StaffPlusPlus.get(), () -> {
-            player.getWorld().setGameRule(GameRule.ANNOUNCE_ADVANCEMENTS, false);
-        });
-
         for (String criteria : advancement.getCriteria()) {
             player.getAdvancementProgress(advancement).revokeCriteria(criteria);
         }
 
+        Bukkit.getGlobalRegionScheduler().execute(StaffPlusPlus.get(), () -> {
+            if (!(player.getWorld().getGameRuleValue(GameRule.SHOW_ADVANCEMENT_MESSAGES) || announceGameruleDisabledByUs)) return;
+            // There is no better way to do it than to disable the gamerule and then enable it
+            announceGameruleDisabledByUs = true;
+            player.getWorld().setGameRule(GameRule.ANNOUNCE_ADVANCEMENTS, false);
+        });
+
+
+        
         // Enable gamerule after the event
         new WrappedRunnable() {
             @Override
             public void run() {
-                player.getWorld().setGameRule(GameRule.ANNOUNCE_ADVANCEMENTS, true);
+                player.getWorld().setGameRule(GameRule.SHOW_ADVANCEMENT_MESSAGES, true);
                 announceGameruleDisabledByUs = false;
             }
         }.runTask(StaffPlusPlus.getScheduler());